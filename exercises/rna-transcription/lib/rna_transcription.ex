3d55e76bb44bb97bcfacfc800a6b126192a63efb
Monluxfer <shibarome@gmail.com>
2020-05-15 21:02:12 +0300

    statr

--- exercises/rna-transcription/lib/rna_transcription.ex
+++ exercises/rna-transcription/lib/rna_transcription.ex
@@ -1,13 +1,20 @@
 defmodule RnaTranscription do
-  @doc """
-  Transcribes a character list representing DNA nucleotides to RNA
+    @doc """
+    Transcribes a character list representing DNA nucleotides to RNA
 
-  ## Examples
+    ## Examples
 
-  iex> RnaTranscription.to_rna('ACTG')
-  'UGAC'
-  """
-  @spec to_rna([char]) :: [char]
-  def to_rna(dna) do
-  end
+    iex> RnaTranscription.to_rna('ACTG')
+    'UGAC'
+    """
+    @spec to_rna([char]) :: [char]
+    def to_rna(dna) do
+        dna
+        |> Enum.map(fn x -> case x do
+            71 -> 67
+            67 -> 71
+            65 -> 85
+            84 -> 65
+        end end)
+    end
 end
--- exercises/word-count/lib/word_count.ex
+++ exercises/word-count/lib/word_count.ex
@@ -1,10 +1,19 @@
 defmodule WordCount do
-  @doc """
-  Count the number of words in the sentence.
+    @ascii_punctuation ~r/!|"|\#|\$|%|&|'|\(|\)|\*|\+|,|\.|\/|:|;|<|=|>|\?|@|\[|\\|]|\^|_|`|\{|\||}|~/
 
-  Words are compared case-insensitively.
-  """
-  @spec count(String.t()) :: map
-  def count(sentence) do
-  end
+    @doc """
+    Count the number of words in the sentence.
+
+    Words are compared case-insensitively.
+    """
+    @spec count(String.t()) :: map
+    def count(sentence) do
+        sentence
+        |> String.downcase()
+        |> String.replace(@ascii_punctuation, " ")
+        |> String.split(" " ,trim: true)
+        |> Enum.map_reduce(%{}, fn x, acc -> {x ,Map.update(acc, x, 1, &(&1 + 1))} end)
+        |> Tuple.to_list()
+        |> List.last()
+    end
 end
